6390c138aa7f808f027c941116afa2030e75ad8e
Tolik <Tolik@MAYZERPC>
2024-03-20 06:16:41 +0300

    Добавьте файлы проекта.

---
+++ C++Project/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+
+
+int main()
+{	
+	for (int i = 1; i <= 4; i++) {
+		for (int j = 1; j <= 5; j++)
+			cout << "2\t";
+		cout << endl;
+	}
+}
